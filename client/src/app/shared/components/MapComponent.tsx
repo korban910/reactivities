e08bc1df50140edf1246a9379cda8a9faa72da6a
korban <korban.torsun@gmail.com>
2026-03-09 07:30:37 -0400

    End of section 22

--- client/src/app/shared/components/MapComponent.tsx
+++ client/src/app/shared/components/MapComponent.tsx
@@ -1,30 +1,27 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import 'leaflet/dist/leaflet.css';
+import "leaflet/dist/leaflet.css";
+import { Icon } from "leaflet";
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
 
 type MapComponentProps = {
   position: [number, number];
   venue: string;
-}
+};
 
-const MapComponent = (
-  {
-    position,
-    venue,
-  } : MapComponentProps
-) => {
-
+const MapComponent = ({ position, venue }: MapComponentProps) => {
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{height: '100%'}}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={position}>
-        <Popup>
-          {venue}
-        </Popup>
+    <MapContainer
+      center={position}
+      zoom={13}
+      scrollWheelZoom={false}
+      style={{ height: "100%" }}
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <Marker position={position} icon={new Icon({ iconUrl: markerIconPng })}>
+        <Popup>{venue}</Popup>
       </Marker>
     </MapContainer>
-  )
-}
+  );
+};
 
 export default MapComponent;
