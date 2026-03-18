ccdb3525c60ca32bb40806d4ddc53406df47963b
korban <korban.torsun@gmail.com>
2026-03-18 07:56:11 -0400

    clean up zod and meterial v7

--- client/src/lib/schemas/loginSchema.ts
+++ client/src/lib/schemas/loginSchema.ts
@@ -1,13 +1,12 @@
 import { z } from "zod";
 
 const loginSchema = z.object({
-  email: z.string().email(),
-  password: z.string({ error : "password is required"}).min(6, { message: "password is required" } ),
+  email: z.email(),
+  password: z
+    .string({ error: "password is required" })
+    .min(6, { message: "password is required" }),
 });
 
 type LoginSchema = z.infer<typeof loginSchema>;
 
-export {
-  loginSchema,
-  type LoginSchema
-}
+export { loginSchema, type LoginSchema };
